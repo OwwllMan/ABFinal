e5e322b6e52cda4c1421c040dcba66d6b1c74a65
BigBiceps <39001414+OwwllMan@users.noreply.github.com>
2018-09-08 14:49:55 +0200

    Add files via upload

---
+++ ABFinal-indev/JS/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#fade").fadeIn(1000);   
+});
+$(document).ready(function() {
+    
+    // à chaque scroll
+    $(window).scroll( function(){
+    
+        // va interroger la locatlisation de chaque élement 
+        $('.hide').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            //si l'objet est
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+}); 
+$(document).ready(function(){
+    $(".main-navToggle").click(function(){
+        (".main-nav").removeClass(".main-nav-open");
+    });
+});
